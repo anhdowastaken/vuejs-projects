a4bda5381f793edf63950056b3dddcf158032e74
Anh Do <doducanh2710@gmail.com>
2018-05-31 15:50:51 +0700

    [Vue Mastery][Intro to Vue.js] Lesson 5 - Event Handling
    Signed-off-by: Anh Do <doducanh2710@gmail.com>

--- main.js
+++ main.js
@@ -13,12 +13,28 @@ var app = new Vue({
             {
                 variantId: 2234,
                 variantColor: "green",
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2234,
                 variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
         sizes: [38, 39, 40, 41, 42],
-    }
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1;
+        },
+        removeFromCart: function() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
+        },
+        updateProduct: function(variantImage) {
+            this.image = variantImage;
+        }
+    },
 });
