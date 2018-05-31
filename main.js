09e3abeba4e7a62828d2020da3682881315891a4
Anh Do <doducanh2710@gmail.com>
2018-05-31 15:12:52 +0700

    [Vue Mastery][Intro to Vue.js] Lesson 4 - List Rendering
    Signed-off-by: Anh Do <doducanh2710@gmail.com>

--- main.js
+++ main.js
@@ -7,6 +7,18 @@ var app = new Vue({
         // link: 'https://www.dropbox.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0',
         // inStock: true,
         inventory: 100,
-        onSale: true,
+        onSale: false,
+        details: ['80% cotton', '20% polyester', "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+            },
+            {
+                variantId: 2234,
+                variantColor: "blue",
+            }
+        ],
+        sizes: [38, 39, 40, 41, 42],
     }
 });
